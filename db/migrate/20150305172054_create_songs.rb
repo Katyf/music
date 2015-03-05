e4c64aa68097f907d50de2ab3b6cf868345ed228
Ashlinn Brennan <asbren13@gmail.com>
2015-03-05 12:21:40 -0500

    changed migration files and migrated database

--- db/migrate/20150305172054_create_songs.rb
+++ db/migrate/20150305172054_create_songs.rb
@@ -2,8 +2,10 @@ class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string :title
-      t.string :artist
+      t.string :album
       t.string :duration
+      t.belongs_to :album, index: true
     end
+    add_foreign_key :songs, :albums
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150305172054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "albums", force: :cascade do |t|
+    t.string "title"
+    t.string "artist"
+    t.string "price"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string  "title"
+    t.string  "album"
+    t.string  "duration"
+    t.integer "album_id"
+  end
+
+  add_index "songs", ["album_id"], name: "index_songs_on_album_id", using: :btree
+
+  add_foreign_key "songs", "albums"
 end
